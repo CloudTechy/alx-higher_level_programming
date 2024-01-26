4ff34a984c6ee4de38c450e5e4cf8e220efdeb85
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-01-26 04:16:38 -0800

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from add_o import add
+if __name__ == "__main__":
+    from add_0 import add
 
-""" imports the add module """
+    """ imports the add module """
 
-a = 1
-b = 2
-sum = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, sum))
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
