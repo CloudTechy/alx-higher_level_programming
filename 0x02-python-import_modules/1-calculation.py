b2f11ce67c1d80f732881f8e82b5c5121f5f502a
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-01-26 04:53:48 -0800

    fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{:s} + {:s} = {:s}".format(a, b, add(a, b)))
-    print("{:s} - {:s} = {:s}".format(a, b, sub(a, b)))
-    print("{:s} * {:s} = {:s}".format(a, b, mul(a, b)))
-    print("{:s} / {:s} = {:s}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
