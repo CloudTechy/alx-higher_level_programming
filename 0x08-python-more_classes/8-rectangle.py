eb53135f4877e4db7abe3b61872da507ce734f84
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-01-31 21:28:20 -0800

    done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -97,7 +97,8 @@ class Rectangle:
                     rect_1 (Rectangle): The first Rectangle.
                     rect_2 (Rectangle): The second Rectangle.
                 Raises:
-                    TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+                    TypeError: If either of rect_1 or
+                    rect_2 is not a Rectangle.
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
