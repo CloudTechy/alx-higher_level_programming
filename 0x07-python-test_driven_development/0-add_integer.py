404305cea7a00981b1771ea913ed7bf90aa4d59d
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-03 04:36:49 -0800

    fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" Add Integer Module """
+
 
 def add_integer(a, b=98):
     """ adds two numbers
@@ -13,10 +15,9 @@ def add_integer(a, b=98):
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
-        
         raise TypeError("a must be an integer")
-    
-    if not isinstance(b, int) and not isinstance(b, float):
 
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
