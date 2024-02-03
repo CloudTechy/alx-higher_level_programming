c842bd6db69bc9ee474ec548f48859bd4ef2fd6a
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-03 06:01:58 -0800

    done

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Divides all element of a matrix with a given number """
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides a matrix by div argument
+
+    args:
+        matrix (nested list): a matrix
+        div (int): divisible operand
+    Returns:
+        matrix: returns a nested list of the divided matrix
+    """
+    row_length = set(len(row) for row in matrix)
+    if len(row_length) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(elem / div, 2) for elem in row]
+        new_matrix.append(new_row)
+    return new_matrix
