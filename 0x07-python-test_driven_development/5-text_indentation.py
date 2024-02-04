c716881ffbb0a0fe4485ada9ddac5546758ab871
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-03 22:21:26 -0800

    prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Module that prints 2 lines after some character """
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines after
+    each of these characters: ., ? and :
+
+    Args:
+        text(str): text to break into 2 lines
+    Returns:
+        (NONE): prints out broken texts
+    """
+
+    if not text:
+        return
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    buf = text.split(".")
+    main = ("\n\n").join([x.strip() for x in buf])
+    buf = main.split("?")
+    main = ("\n\n").join([x.strip() for x in buf])
+    buf = main.split(":")
+    main = ("\n\n").join([x.strip() for x in buf])
+    print(main)
