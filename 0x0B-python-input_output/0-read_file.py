8334877b44c59d35fd53421da9aa302a135aa043
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-06 15:58:40 -0800

    dond

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
             filename (str): name of file
     """
 
-    with open(filename, r, encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
