a49ebee54131d8378c3b205ef94cf0358744cccd
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-05 16:55:54 -0800

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" sorts list """
+
+
+class MyList(list):
+    """ A class List """
+    
+    def print_sorted(self):
+        """ prints sorted list """
+
+        return (self.sort())
