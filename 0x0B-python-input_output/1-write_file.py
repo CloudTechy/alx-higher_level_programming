523081789586938e9459a3b18b88ec37d722a835
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-06 16:04:50 -0800

    writes to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that writes to a file """
+
+
+    def write_file(filename="", text=""):
+    """ reads a file and prints to stdout
+
+        Args:
+            filename (str): name of file
+            text(str): content to write
+    """
+
+    with open(filename, w, encoding="utf-8") as f:
+        return f.write(text)
