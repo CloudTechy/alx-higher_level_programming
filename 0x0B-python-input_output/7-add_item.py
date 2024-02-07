e328c8fe24597647ba451ffb6662f87462420227
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-06 17:18:00 -0800

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,11 +4,12 @@
 
 
 import sys
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
+if __name__ == "__main__":
+    save = __import__("5-save_to_json_file").save_to_json_file
+    load = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    load("add_item.json")
-except FileNotFoundError:
-    args = sys.argv[1:]
-    save(args, filename="add_item.json")
+    try:
+        load("add_item.json")
+    except FileNotFoundError:
+        args = sys.argv[1:]
+        save(args, filename="add_item.json")
