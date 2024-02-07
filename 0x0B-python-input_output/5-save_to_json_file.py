b614a2de98f1d5ada430b3618efbffbcee23edb1
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-06 16:30:31 -0800

    creates a json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Module that creates a json file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ serialize my_obj and save to filename
+
+        Args:
+            my_obj (any): obj to serialize
+            filename (str): name of file
+    """
+    jsonTxt = json.dumps(my_obj)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(jsonTxt)
