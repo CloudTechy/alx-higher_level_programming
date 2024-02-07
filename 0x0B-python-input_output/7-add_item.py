6f3773516e12577c0270cac8b17d0b89297988fe
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-06 16:56:49 -0800

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,5 +8,5 @@ save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 
 args = sys.argv[1:]
-save(add_item.json)
-return load(add_item.json)
+save(args, filename="add_item.json")
+#print(load("add_item.json"))
