8c260229cd7999ec323858c4479aae7411cbc655
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-08 15:28:08 -0800

    done

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,12 +3,11 @@
 def print_list_integer(my_list=[]):
     """ Prints list of integeres
 
-        args: 
+        args:
             my_list (list): List of integers
         Returns:
             None: prints the numbers
     """
-    for num in my_list:
-        print ("{:d}".format(num))
 
-print_list_integer([1,2,3])
+    for num in my_list:
+        print("{:d}".format(num))
