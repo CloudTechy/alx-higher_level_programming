e567965fc8c95698953ed21d62088a24b2f7dfd9
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-09 02:49:26 -0800

    done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,4 +4,4 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
         for i in range(len(my_list)):
-            new_list[i] = True if my_list[i] % 2 == 0 else False
+            new_list.append(True if my_list[i] % 2 == 0 else False)
