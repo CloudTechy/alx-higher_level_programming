0ba0324449c25524f6492dfa0c7a0415c0054b2f
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-02-14 02:03:19 -0800

    fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """ Base of all classess in the models """
 
+
 class Base:
     """Defines the base class """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """Initializes the base class
+
+            args:
+                self (object) : instance of base class
+                id (int): integer number
+        """
+
         if id is not None:
             self.id = id
         else:
-           Base.__nb_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
