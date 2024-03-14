b174c40a9a7b328ff7af77c0f3ce83d69a7e823d
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-13 18:05:05 -0700

    done

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,4 @@
 
 USE hbtn_0d_usa;
 
-SELECT id, name  FROM cities WHERE cities.state_id = (
-SELECT id FROM states WHERE name = 'California'
-) ORDER BY cities.id ASC;
+SELECT id, name  FROM cities WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
