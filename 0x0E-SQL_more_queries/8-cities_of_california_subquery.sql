336f9ad1d0703cac6f97a0634764443f74031d41
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-13 17:48:32 -0700

    done.

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+
+USE hbtn_0d_usa;
+
+SELECT * FROM cities WHERE states.id = (
+SELECT id FROM  states WHERE name = 'California';
+)
