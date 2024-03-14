0186bbb5c9bababcd5a5b1e934df7e75fd028f8b
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-13 18:13:49 -0700

    dome

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- lists all rows of a column in a database
+SELECT cities.id cities.name states.name FROM cities, states ORDER BY cities.id ASC;
