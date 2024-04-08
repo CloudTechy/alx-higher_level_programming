ac89ec70c4bc05a437cbb676361f2e1618acf4cf
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-08 15:22:12 -0700

    done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const argl = process.argv.length;
+
+if (argl === 2) console.log('No argument');
+else if (argl === 3) console.log('Argument found');
+else console.log('Arguments found');
