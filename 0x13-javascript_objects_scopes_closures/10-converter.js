55798a8891f9acf2b81f8430f45b847bf9834669
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-04-09 23:57:55 +0100

    done

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,17 +3,4 @@
 const converter = (base) => {
   return (num) => num.toString(base);
 };
-
-let myConverter = converter(10);
-
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
-
-myConverter = converter(16);
-
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
-
 exports.converter = converter;
