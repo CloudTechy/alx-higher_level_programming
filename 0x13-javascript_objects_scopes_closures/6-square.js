a3fd7058cd8b08f9a311630421b1015b8c3b1b3f
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-09 14:37:05 -0700

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,19 +3,14 @@
 const SquareC = require('./5-square.js');
 
 class Square extends SquareC {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
-    if (c) {
-      let h = this.height;
-      while (h > 0) {
-        console.log(c.repeat(this.width));
-        h--;
-      }
-    } else {
-      this.print();
+    if (c === undefined) {
+      c = 'X';
+    }
+    let h = this.height;
+    while (h > 0) {
+      console.log(c.repeat(this.width));
+      h--;
     }
   }
 }
