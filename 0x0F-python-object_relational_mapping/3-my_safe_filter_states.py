14180b036af4db4fcefc3f06c7084606dfd06cb7
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-14 02:02:28 -0700

    doene

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if (__name__ == '__main__'):
             )
     cur = con.cursor()
     cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY %s ORDER BY states.id""",(argv[4],))
+                LIKE BINARY %s ORDER BY states.id""", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
