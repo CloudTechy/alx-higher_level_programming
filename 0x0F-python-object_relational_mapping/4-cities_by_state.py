2e4036015e5357c10ae97fc24f3ddd1befcda4c1
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-14 02:12:25 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" list all cities and states from database """
+
+import sys
+import MySQLdb
+
+argv = sys.argv
+if (__name__ == '__main__'):
+    con = MySQLdb.connect(
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3],
+                host='localhost',
+                port=3306
+            )
+    cur = con.cursor()
+    query = cur.execute('SELECT states.id  FROM states ORDER BY `id` asc;')
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+    cur.close()
+    con.close()
