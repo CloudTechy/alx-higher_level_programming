7ceaedbba5ffaf03cdc79fe69247f9c01751e859
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-04-14 11:34:39 +0100

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+""" filter cities from database """
+
 import sys
+import MySQLdb
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
+argv = sys.argv
+if (__name__ == '__main__'):
+    con = MySQLdb.connect(
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3],
+                host='localhost',
+                port=3306
+            )
+    cur = con.cursor()
     cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
+    toList = list(row[0] for row in rows)
+    print(*toList, sep=", ")
     cur.close()
-    db.close()
+    con.close()
