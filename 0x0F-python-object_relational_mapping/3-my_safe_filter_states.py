5a8081d5cafb911afec5e83d19470d7033b63c85
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-14 02:01:17 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" filter cities from database """
+
+import sys
+import MySQLdb
+
+argv = sys.argv
+if (__name__ == '__main__'):
+    con = MySQLdb.connect(
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3],
+                host='localhost',
+                port=3306
+            )
+    cur = con.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY %s ORDER BY states.id""",(argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
