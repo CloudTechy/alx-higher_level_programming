004804ae3534c43133ac40c03e4aa01f09cc1f8f
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-14 01:48:19 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" list all cities from database """
+
+import sys
+import MySQLdb
+
+argv = sys.argv
+if (__name__ == '__main__'):
+    con = MySQLdb.connect(
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3],
+                host='localhost',
+                port=3306
+            )
+    cur = con.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                = %s ORDER BY states.id""", (argv[4],))
+    row = cur.fetchone()
+    print(row)
+    cur.close()
+    con.close()
