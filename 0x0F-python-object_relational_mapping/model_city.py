33310ab4f97d17bebf0ca059dcca27b283917b8b
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-15 05:43:48 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" create state model """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ defines a state class orm """
+    __tablename__ = 'cities'
+    id = Column(
+            Integer,
+            primary_key=True,
+            unique=True,
+            nullable=False
+            )
+    name = Column(
+            String(128),
+            nullable=False
+            )
+    state_id = Column(
+            Integer,
+            ForeignKey("states.id"),
+            nullable=False
+            )
