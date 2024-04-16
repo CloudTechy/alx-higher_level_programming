2d7257a3dfdf8358a39012613ba36042ca1a573c
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-15 21:55:19 -0700

    done

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ create state model """
 
+from relationship_state import State
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ create state model """
 
+from relationship_city import City
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
