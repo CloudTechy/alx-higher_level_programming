a9f5818267fa20007fe51467c9338c58ce668b33
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-04-26 21:59:41 -0700

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
+    if "ok" in r.text.lower():  # Check if "ok" is present anywhere in the response      
+        print("\t- Content:", r.text[:50])  # Print the first 50 characters of the response
+
