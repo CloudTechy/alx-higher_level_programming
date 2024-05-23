f0766cc11801a48a6aa6016863cd4dcd6c3aa1b5
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-05-23 15:58:03 -0700

    adds statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// display the status code of a GET request.
+
+const req = require('request');
+const url = process.argv[2];
+req(url, (err, status) => {
+  if (err) console.log(err);
+  if (status) console.log("code: ", status.statusCode);
+});
