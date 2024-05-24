13f0b6ddef6d1b9fa6601396844cc7326a5c1e98
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-05-23 17:23:21 -0700

    accomodates more searcjed character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,10 +7,10 @@ req(url, (err, status, body) => {
   if (err) console.log(err);
   if (status.statusCode === 200) {
     let sum = 0;
-    const castUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+    const castUrl = 'people/18/';
     const films = JSON.parse(body);
     for (const film of films.results) {
-      if (film.characters && film.characters.includes(castUrl)) sum++;
+      if (film.characters && film.characters.some(character => character.includes(castUrl))) sum++;
     }
     console.log(sum);
   }
